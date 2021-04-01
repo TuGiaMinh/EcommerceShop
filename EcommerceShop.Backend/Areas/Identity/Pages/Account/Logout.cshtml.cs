831d2a2e51388ecb4d3fec6ae4346dce03a926c8
TuGiaMinh <64824448+TuGiaMinh@users.noreply.github.com>
2021-04-01 11:10:30 +0700

    Fix redirect logout to customersite

--- EcommerceShop.Backend/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ EcommerceShop.Backend/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -8,29 +8,45 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using IdentityServer4.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcommerceShop.Backend.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
+        private readonly IIdentityServerInteractionService _interaction;
         private readonly SignInManager<User> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger, IIdentityServerInteractionService interaction)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _interaction = interaction;
         }
-
-        public void OnGet()
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public class InputModel
         {
+            [Required]
+            public string LogoutId { get; set; }
+        }
+        public void OnGet(string logoutId)
+        {
+            Input = new InputModel { LogoutId = logoutId };
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+            var logout = await _interaction.GetLogoutContextAsync(Input.LogoutId);
+            if (logout != null && !string.IsNullOrWhiteSpace(logout.PostLogoutRedirectUri))
+            {
+                return Redirect(logout.PostLogoutRedirectUri);
+            }
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
