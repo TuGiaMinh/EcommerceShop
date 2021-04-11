f53a93f73fa9f17910886d64be8c62763c71d893
TuGiaMinh <64824448+TuGiaMinh@users.noreply.github.com>
2021-04-11 18:33:09 +0700

    Add item amount in layout

---
+++ EcommerceShop.CustomerSite/Components/ShoppingCartViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using EcommerceShop.CustomerSite.Extensions;
+using EcommerceShop.Shared.Order;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceShop.CustomerSite.Components
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Cart cart = HttpContext.Session.Get<Cart>("ssShoppingCart");
+            return View(cart);
+        }
+    }
+}
