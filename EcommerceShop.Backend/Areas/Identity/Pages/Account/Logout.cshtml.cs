ec955a3cc7e10b4dfe6e7a1c0bfafb1e2b7f3a05
TuGiaMinh <64824448+TuGiaMinh@users.noreply.github.com>
2021-04-27 09:30:40 +0700

    Fix : react signout redirect

--- EcommerceShop.Backend/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ EcommerceShop.Backend/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using IdentityServer4.Services;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Configuration;
 
 namespace EcommerceShop.Backend.Areas.Identity.Pages.Account
 {
@@ -17,14 +18,16 @@ namespace EcommerceShop.Backend.Areas.Identity.Pages.Account
     public class LogoutModel : PageModel
     {
         private readonly IIdentityServerInteractionService _interaction;
+        private readonly IConfiguration _configuration;
         private readonly SignInManager<User> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger, IIdentityServerInteractionService interaction)
+        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger, IIdentityServerInteractionService interaction, IConfiguration configuration)
         {
             _signInManager = signInManager;
             _logger = logger;
             _interaction = interaction;
+            _configuration = configuration;
         }
         [BindProperty]
         public InputModel Input { get; set; }
@@ -40,13 +43,19 @@ namespace EcommerceShop.Backend.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
+
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
             var logout = await _interaction.GetLogoutContextAsync(Input.LogoutId);
+            if (logout.ClientIds?.ElementAt(0) == "react")
+            {
+                return Redirect(_configuration["IdentityServerConfig:Clients:React:RedirectLogout"]);
+            }
             if (logout != null && !string.IsNullOrWhiteSpace(logout.PostLogoutRedirectUri))
             {
                 return Redirect(logout.PostLogoutRedirectUri);
             }
+           
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
